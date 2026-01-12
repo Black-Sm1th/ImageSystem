3a802cfa1011dd5e8287fc6084b33f5df5271764
Black-Sm1th <714557144@qq.com>
2026-01-12 14:36:57 +0800

    fix：取消键盘触发interactor事件

--- ImageSystem/main.cpp
+++ ImageSystem/main.cpp
@@ -94,7 +94,7 @@ int main(int argc, char* argv[])
     QGuiApplication app(argc, argv);
 
     // 安装全局键盘监听（不依赖焦点）
-    app.installEventFilter(new GlobalKeyFilter(&app));
+    //app.installEventFilter(new GlobalKeyFilter(&app));
 
     // 注册四个视图类型
     qmlRegisterType<AxialVtkItem>("com.vtk.dicom", 1, 0, "AxialView");
